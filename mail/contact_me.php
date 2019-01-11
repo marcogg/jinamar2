231b3adc733c7fbc708d79afc0dd7c001adef09f
Marco García <marcogarcia.gon@gmail.com>
2019-01-11 17:16:33 -0600

    Pagina INglés y Español
    Falta checar el formulario

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,24 +2,24 @@
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
+   empty($_POST['subject'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Formulario vacío!";
    return false;
    }
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$phone = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'marcogarcia.gon@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Nueva entrada de Formulario en JIRAMAR.COM.MX:  $name";
+$email_body = "Ha recibido una nueva entrada del formulario de contacto del sitio web.\n\n"."Datos proporcionados:\n\nNombre: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
