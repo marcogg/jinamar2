a2fe579d089039ff2e1d05d18936ae667ae7ec99
marcogg <marcogarcia.gon@gmail.com>
2019-01-25 01:34:36 -0600

    Sitio temrinado
    todo OK

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'marcogarcia.gon@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@dimarcogarcia.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Nueva entrada de Formulario en JIRAMAR.COM.MX:  $name";
 $email_body = "Ha recibido una nueva entrada del formulario de contacto del sitio web.\n\n"."Datos proporcionados:\n\nNombre: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
